1e6a84c805637ef0b1e5e79184b7eadaebfde499
Brenda <brennnda.gonzalezq@gmail.com>
2019-10-21 21:19:55 -0500

    Components for SignUp

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 import App from 'next/app'
 
 import '../styles.scss'
+import Navbar from '../components/Navbar'
+
+class Layout extends Component {
+  render () {
+    return (
+      <div className='layout'>
+        <Navbar />
+        <div className='container'>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
 
 class MyApp extends App {
   render () {
     const { Component, pageProps } = this.props
-    return <Component { ...pageProps } />
+    return (
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    )
   }
 }
 
