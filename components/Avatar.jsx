dade5a4b14eb90d12db40f4af164fd4c62de69ce
Carl-code <arimatea865@gmail.com>
2019-10-26 02:43:54 -0500

    CREATE: medals image

--- components/Avatar.jsx
+++ components/Avatar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Avatar (props) {
   return (
     <figure className='image is-96x96'>
-      <img className='is-circular is-avatar' src={props.image} />
+      <img className='is-avatar' src={props.image} />
     </figure>
   )
 }
