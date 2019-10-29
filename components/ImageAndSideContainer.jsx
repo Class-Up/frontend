20314e5df14bded51940b3d6e4f885b70432b2ca
Carl-code <arimatea865@gmail.com>
2019-10-28 18:46:19 -0600

    ADD: Responsive to image

--- components/ImageAndSideContainer.jsx
+++ components/ImageAndSideContainer.jsx
@@ -5,9 +5,9 @@ function ImageAndSideContainer (props) {
   return (
     <div className='columns has-text-centered'>
       <div className='column is-half-desktop'>
-      <figure className='image'>
-        <img src={props.image} alt={props.image} />
-      </figure>
+        <figure className='image'>
+          <img src={props.image} alt={props.image} />
+        </figure>
       </div>
       <div className='column is-half-desktop'>
         <div className='column'>
