59761ba26af21d6ca5992ea202fd83ae912e5446
Rurick Maqueo <skintigthvader@gmail.com>
2019-10-31 16:58:39 -0600

    fix conflicts

--- components/MedalsList.jsx
+++ components/MedalsList.jsx
@@ -1,12 +1,9 @@
 import React from 'react'
 
 import Medals from './Medals'
-import Title from './Title'
 
 function MedalsList (props) {
   return (
-<<<<<<< HEAD
-<<<<<<< Updated upstream
     <>
       {props.medals.map((medal, index) => {
         return (
@@ -17,37 +14,6 @@ function MedalsList (props) {
         )
       })}
     </>
-=======
-=======
->>>>>>> develop
-    <div className='medal-list'>
-      <Title
-        size='5'
-        text='Tus Medallas'
-      />
-<<<<<<< HEAD
-      <div className='columns is-multiline is-mobile'>
-        {props.medals.map((medal, index) => {
-          return (
-            <div className='column is-one-third' key={index}>
-=======
-      <div className="columns is-multiline is-mobile">
-        {props.medals.map((medal, index) => {
-          return (
-            <div className="column is-one-third" key={index}>
->>>>>>> develop
-              <Medals
-                image={medal.name}
-              />
-            </div>
-          )
-        })}
-      </div>
-    </div>
-<<<<<<< HEAD
->>>>>>> Stashed changes
-=======
->>>>>>> develop
   )
 }
 
