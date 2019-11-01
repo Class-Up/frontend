fc580c6e4ac3968a6161ae02f62cccd9eee8b55c
Carl-code <arimatea865@gmail.com>
2019-11-01 04:07:18 -0600

    ADD: Modal onluy

--- components/Modal.jsx
+++ components/Modal.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
 
 function Modal (props) {
+
+function handleClick(){
+  document.getElementById('modalCard').style.display='none'
+}
+
   return (
-    <div className='modal is-active'>
+    <div id='modalCard' className='modal'>
       <div className='modal-background' />
       <div className='modal-card'>
         <header className='modal-card-head'>
           <p className='modal-card-title'>{props.title}</p>
-          <button className='delete' aria-label='close' />
+          <button className='delete' aria-label='close' onClick={handleClick} />
         </header>
         <div className='columns'>
           <section className='modal-card-body'>
